b9ba0d1ba55054466ce65d530b6f8ddc371c6c69
Ashutosh <aipanhouse15@gmail.com>
2026-03-31 14:50:38 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'assurefintech.com' }],
+        destination: 'https://www.assurefintech.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
